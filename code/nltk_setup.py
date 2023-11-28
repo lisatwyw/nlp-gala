662be0b98fe5de68b7d5b847461d6c5ebca9c807
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-11-27 17:25:06 -0800

    Update nltk_setup.py

--- code/nltk_setup.py
+++ code/nltk_setup.py
@@ -1,1 +1,15 @@
-import nltk; nltk.download('punkt')
+import nltk
+
+nltk.download('punkt')
+
+import nltk.data
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle') 
+
+
+# LEALLA model
+import torch
+from transformers import BertModel, BertTokenizerFast
+
+tokenizer = BertTokenizerFast.from_pretrained("setu4993/LEALLA-base")
+model = BertModel.from_pretrained("setu4993/LEALLA-base")
+model = model.eval()

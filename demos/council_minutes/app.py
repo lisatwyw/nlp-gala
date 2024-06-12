9eb992a4eae8040196af87a50e24301101aeb68f
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-06-12 15:06:12 -0700

    Update app.py

--- demos/council_minutes/app.py
+++ demos/council_minutes/app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import pandas as pd
+from st_pages import Page, show_pages, add_page_title  # allow multipages
+from pathlib import Path
+from glob import glob 
 
-dat = pd.read_csv('output/bby.csv')
+import numpy as np
+
+parent_dir = str( Path(__file__).parents[0] )
+gparent_dir= str( Path(__file__).parents[1] )
+data_dir = gparent_dir +  '/data/' 
+
+dat = pd.read_csv( gparent_dir + 'output/bby.csv')
 st.dataframe(dat)
 
 

55ec655f6ba2e1d8bea88f969b1d3b57ee228556
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-06-12 15:37:12 -0700

    Update app.py

--- demos/council_minutes/app.py
+++ demos/council_minutes/app.py
@@ -5,16 +5,24 @@ from pathlib import Path
 import os, sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
+print( current_dir )
+print( parent_dir )
+
+parent_dir = str( Path(__file__).parents[0] )
+gparent_dir= str( Path(__file__).parents[1] )
+print( parent_dir )
+print( gparent_dir )
+
+sys.path.append(current_dir)
 sys.path.append(parent_dir)
 
 # ================================ Widgets =============================== 
 st.title( 'Civil discourse' )
 
-parent_dir = str( Path(__file__).parents[0] )
-gparent_dir= str( Path(__file__).parents[1] )
+
+
 st.write(gparent_dir)
 
-print( gparent_dir )
 
 show_pages(
     [

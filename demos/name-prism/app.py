9e2f8439be701247a5d1dc12e167b351dc3b315d
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-06-28 21:58:21 -0700

    Update app.py

--- demos/name-prism/app.py
+++ demos/name-prism/app.py
@@ -29,3 +29,25 @@ show_pages(
 f = parent_dir + '/readme.md'
 mkd = Path( f ).read_text()
 st.markdown( mkd )
+
+
+filepath = parent_dir + '/data/Popular_Baby_Names.csv'
+df = pd.read_csv( Path(filepath ))
+
+mkd = '''
+## Ethnicities in this dataset
+- Data source: https://catalog.data.gov/dataset/popular-baby-names
+- Size of dataset:
+'''
+st.markdown( mkd )
+st.write( df.shape )
+
+
+fig = px.histogram( df, x = 'Ethnicity',  title=f'{1}' )
+fig.update_layout(hovermode="x unified")
+
+#fig.update_xaxes(showspikes=True, spikemode="across")
+#fig.update_yaxes(showspikes=True, spikemode="across")
+
+st.plotly_chart( fig )
+

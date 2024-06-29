0e30ba5742886fe2322c2c551deb3bf7fa871fa3
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-06-28 21:13:27 -0700

    Create app.py

---
+++ demos/whoiswho/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from st_pages import Page, show_pages, add_page_title  # allow multipages
+from pathlib import Path
+
+import os, sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+print( current_dir )
+print( parent_dir )
+
+parent_dir = str( Path(__file__).parents[0] )
+gparent_dir= str( Path(__file__).parents[1] )
+print( parent_dir )
+print( gparent_dir )
+
+sys.path.append(current_dir)
+sys.path.append(parent_dir)
+
+# ================================ Widgets =============================== 
+st.title( 'Civil discourse' )
+show_pages(
+    [
+        Page(Path( current_dir, "app.py"), "Who is Who", "🏠", "Who is Who"),
+        
+        
+    ]
+)

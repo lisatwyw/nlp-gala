8b372d57dd8f1f5f8e202040270d0bbebf91e681
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-06-28 21:54:17 -0700

    Update data.py

--- demos/name-prism/data.py
+++ demos/name-prism/data.py
@@ -9,38 +9,6 @@ import plotly.express as px
 import os, sys
 import numpy as np
 
-parent_dir = str( Path(__file__).parents[0] )
-gparent_dir= str( Path(__file__).parents[1] )
-data_dir = parent_dir +  '/data/' 
-
-
-st.set_page_config(layout="wide")
-st.write(gparent_dir)
-# ============================== read data ==============================
-filepath = parent_dir + '/data/Popular_Baby_Names.csv'
-
-df = pd.read_csv( Path(filepath ))
-
-mkd = '''
-## Ethnicities in this dataset
-- Data source: https://catalog.data.gov/dataset/popular-baby-names
-- Size of dataset:
-'''
-
-st.markdown( mkd )
-st.write( df.shape )
-
-
-fig = px.histogram( df, x = 'Ethnicity',  title=f'{1}' )
-fig.update_layout(hovermode="x unified")
-
-#fig.update_xaxes(showspikes=True, spikemode="across")
-#fig.update_yaxes(showspikes=True, spikemode="across")
-
-st.plotly_chart( fig )
-
-
-
 import sys, os
 from st_pages import Page, show_pages, add_page_title  # allow multipages
 from streamlit_dynamic_filters import DynamicFilters
@@ -59,24 +27,63 @@ def color_backgroubd_red_column(col):
     return ['background: red' for _ in col]
 
 
-try:
-    S = df.columns 
-    dynamic_filters = DynamicFilters(df,
-                                     filters=S, 
-                                     )
-    with st.sidebar:
-        st.write("Apply filters in any order")
-    dynamic_filters.display_filters(location='sidebar')
-    dynamic_filters.display_df()   
-    new_df = dynamic_filters.filter_df()
-except Exception as e:
-    st.text( e )
 
 
-st.write( 'Quick summary:')
-st.write( new_df.Ethnicity.describe()  )
-try:
-  st.write( new_df['Child\'s First Name'].describe() )
-except:
-  pass
+
+
+parent_dir = str( Path(__file__).parents[0] )
+gparent_dir= str( Path(__file__).parents[1] )
+data_dir = parent_dir +  '/data/' 
+
+
+st.set_page_config(layout="wide")
+st.write(gparent_dir)
+
+# ============================== read data ==============================
+filepath = parent_dir + '/data/Popular_Baby_Names.csv'
+
+df = pd.read_csv( Path(filepath ))
+
+mkd = '''
+## Ethnicities in this dataset
+- Data source: https://catalog.data.gov/dataset/popular-baby-names
+- Size of dataset:
+'''
+
+st.markdown( mkd )
+st.write( df.shape )
+
+
+tabs= st.tabs( ['Filter', 'Summary'] )
+
+with tabs[1]:
+    fig = px.histogram( df, x = 'Ethnicity',  title=f'{1}' )
+    fig.update_layout(hovermode="x unified")
+    
+    #fig.update_xaxes(showspikes=True, spikemode="across")
+    #fig.update_yaxes(showspikes=True, spikemode="across")
+    
+    st.plotly_chart( fig )
+
+
+with tabs[0]:
+    try:
+        S = df.columns 
+        dynamic_filters = DynamicFilters(df,
+                                         filters=S, 
+                                         )
+        with st.sidebar:
+            st.write("Apply filters in any order")
+        dynamic_filters.display_filters(location='sidebar')
+        dynamic_filters.display_df()   
+        new_df = dynamic_filters.filter_df()
+    except Exception as e:
+        st.text( e )
+    
+    st.write( 'Quick summary:')
+    st.write( new_df.Ethnicity.describe()  )
+    try:
+      st.write( new_df['Child\'s First Name'].describe() )
+    except:
+      pass
 

190a371c1d63ca36779d5cec1d97380e44e0fcaa
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-07-02 17:41:18 -0700

    Create data4.py

---
+++ demos/name-prism/data4.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from st_pages import Page, show_pages, add_page_title  # allow multipages
+from pathlib import Path
+import pandas as pd
+import os, sys
+import plotly.express as px
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+print( current_dir )
+print( parent_dir )
+
+parent_dir = str( Path(__file__).parents[0] )
+gparent_dir= str( Path(__file__).parents[1] )
+print( parent_dir )
+print( gparent_dir )
+
+sys.path.append(current_dir)
+sys.path.append(parent_dir)
+
+
+st.set_page_config(layout="wide")
+
+# ================================ Widgets =============================== 
+
+
+st.markdown( '## A possible distribution')
+df = pd.DataFrame( dict( Ethnicity = ['White', 'Chinese','Other',  'South Asian', 'First Nations', 
+                                      'Metis' , 'Latin America/Hispanic', 'Filipino', 'Southeast Asian',
+                                      'Japanese', 'West Asian', 'Black', 'Korean', 'Arab', 'Inuit' ], 
+                        N = [301563,19071,12768,9892,6301,6384,4722,4389,2136,2726,2072,1807,1324,1005,197],
+                        Percentage=[76.46,4.84,3.24,2.51,1.6,1.62,1.2,1.11,.54,.69,.53,0.45,.34,.25,.05,] ))
+st.dataframe( df )
+     

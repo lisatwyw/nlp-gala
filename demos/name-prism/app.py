c13c758222fbeef85052b20b832e4e9735f45ee5
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-07-02 17:40:50 -0700

    Update app.py

--- demos/name-prism/app.py
+++ demos/name-prism/app.py
@@ -28,6 +28,7 @@ show_pages(
         Page(Path( current_dir, "data1.py"), "Popular baby names in US",  ),
         Page(Path( current_dir, "data2.py"), "Data from Crabtree et al.",  ),
         Page(Path( current_dir, "data3.py"), "Data dict WVS", ),
+        Page(Path( current_dir, "data4.py"), "Spk. R1", ),
         
     ]
 )
@@ -37,11 +38,3 @@ if 1:
     mkd = Path( f ).read_text()
     st.markdown( mkd )
 
-    st.markdown( '## A possible distribution')
-    df = pd.DataFrame( dict( Ethnicity = ['White', 'Chinese','Other',  'South Asian', 'First Nations', 
-                                          'Metis' , 'Latin America/Hispanic', 'Filipino', 'Southeast Asian',
-                                          'Japanese', 'West Asian', 'Black', 'Korean', 'Arab', 'Inuit' ], 
-                            N = [301563,19071,12768,9892,6301,6384,4722,4389,2136,2726,2072,1807,1324,1005,197],
-                            Percentage=[76.46,4.84,3.24,2.51,1.6,1.62,1.2,1.11,.54,.69,.53,0.45,.34,.25,.05,] ))
-    st.dataframe( df )
-     

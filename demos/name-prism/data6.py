1d23958fb9be40634c919b0925c7f9c17a910341
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-07-12 22:22:16 -0700

    Create data6.py

---
+++ demos/name-prism/data6.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+from st_pages import Page, show_pages, add_page_title  # allow multipages
+from pathlib import Path
+from glob import glob 
+
+import plotly.express as px
+
+import os, sys
+import numpy as np
+
+import sys, os
+from streamlit_dynamic_filters import DynamicFilters
+
+
+
+parent_dir = str( Path(__file__).parents[0] )
+gparent_dir= str( Path(__file__).parents[1] )
+data_dir = parent_dir +  '/data/' 
+
+st.set_page_config(layout="wide")
+# st.write(gparent_dir)
+
+# ============================== read data ==============================
+
+with open( Path( data_dir, 'vm_age_sex_distr.html') ,'r') as f: 
+    html_data = f.read()
+
+# ================================  widget ================================ 
+
+st.header("Age and sex distribution in visible minority")
+components.html(html_data, scrolling=True, height=500 ) 
